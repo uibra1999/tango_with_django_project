471eddade322effcda87f3216db8bf2271c6a295
Umar Ibrahim <2366908I@student.gla.ac.uk>
2020-01-30 22:17:49 +0000

    Chapter 3 Exercises part 1

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,6 +20,7 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
