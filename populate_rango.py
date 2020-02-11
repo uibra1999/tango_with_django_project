8389630115d4c9f8e5703f3f4fe7ad5effaa5fb5
Umar Ibrahim <2366908I@student.gla.ac.uk>
2020-02-11 01:44:06 +0000

    Done chapters 6,7 and half of 8

--- populate_rango.py
+++ populate_rango.py
@@ -62,7 +62,7 @@ def populate():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
--- rango/admin.py
+++ rango/admin.py
@@ -5,5 +5,17 @@ from rango.models import Category, Page
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name', )}
+
+admin.site.register(Category, CategoryAdmin)
+
+#admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
+
+
+
